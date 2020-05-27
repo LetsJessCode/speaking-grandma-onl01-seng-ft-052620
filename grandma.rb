fe13f0c39b389f7ec87b1e5927928d6b61cc3429
Jessica Shearwood <letsjesscode@gmail.com>
2020-05-27 14:00:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,10 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
-     "I LOVE YOU TOO PUMPKIN!"
-end
-end
-# elsif string != string.upcase
-#   "HUH?! SPEAK UP, SONNY!"
-   
-# else
-#     "NO, NOT SINCE 1938!"
-
+    'I LOVE YOU TOO, SONNY!'
+  elsif string != string.upcase
+    "HUH?! I CAN'T HEAR YOU, SPEAK UP!"
+  else
+    "NOT SINCE 1938!"
+  end
+end 
  
